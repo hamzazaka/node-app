fd0652453e9bbce10811acf54a7277ebbd4ad971
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2022-03-25 11:44:03 -0400

    node js post data

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,29 @@ import express from "express";
 
 const router= express.Router();
 
+const users=[
+    {
+        name:'hamza',
+        age:23
+    },
+    {
+        name:'elon',
+        age:50
+    },
+    {
+        name:'john',
+        age:24
+    },
+]
+
 router.get('/users', function(req,res){
-    res.json({
-        "user":'hamza',
-        "no":2
-    })
+    res.json(users)
+});
+
+router.post('/users',function(req,res){
+    const body=req.body
+    console.log(body);
+
 })
 
 export default router;
